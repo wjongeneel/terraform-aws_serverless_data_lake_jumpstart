5cf6d42875d3ee671ab75715c168a100789cc955
wjongeneel <wjongeneel@users.noreply.github.com>
2024-01-31 20:03:52 +0000

    terraform fmt

--- iam.tf
+++ iam.tf
@@ -1,42 +1,42 @@
 resource "aws_iam_role" "AWSGlueServiceRole-SDL-Jumpstart" {
-    name = "AWSGlueServiceRole-SDL-Jumpstart"
-    assume_role_policy = jsonencode({
-   "Version":"2012-10-17",
-   "Statement":[
+  name = "AWSGlueServiceRole-SDL-Jumpstart"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-         "Effect":"Allow",
-         "Principal":{
-            "Service":"glue.amazonaws.com"
-         },
-         "Action":"sts:AssumeRole"
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "glue.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
       }
-   ]
-})
-        tags = {
-        Name = "${var.project_tag}"
-    }
+    ]
+  })
+  tags = {
+    Name = "${var.project_tag}"
+  }
 }
 
 resource "aws_iam_role_policy" "glue-trust-policy" {
-    name = "glue-trust-policy"
-    role = aws_iam_role.AWSGlueServiceRole-SDL-Jumpstart.id
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:ListBucket"
-            ],
-            "Resource": [
-                "arn:aws:s3:::[your-bucket]-raw/*",
-                "arn:aws:s3:::[your-bucket]-raw",
-                "arn:aws:s3:::[your-bucket]-transformed/*",
-                "arn:aws:s3:::[your-bucket]-transformed"
-            ]
-        }
+  name = "glue-trust-policy"
+  role = aws_iam_role.AWSGlueServiceRole-SDL-Jumpstart.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "arn:aws:s3:::[your-bucket]-raw/*",
+          "arn:aws:s3:::[your-bucket]-raw",
+          "arn:aws:s3:::[your-bucket]-transformed/*",
+          "arn:aws:s3:::[your-bucket]-transformed"
+        ]
+      }
     ]
-})
+  })
 }
--- s3.tf
+++ s3.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "taxi-trip-data-raw" {
-    bucket = "taxi-trip-data-raw"
-    tags = {
-        Name = "${var.project_tag}"
-    }
+  bucket = "taxi-trip-data-raw"
+  tags = {
+    Name = "${var.project_tag}"
+  }
 }
 
 resource "aws_s3_bucket" "taxi-trip-data-transformed" {
-    bucket = "taxi-trip-data-transformed"
-    tags = {
-        Name = "${var.project_tag}"
-    }
+  bucket = "taxi-trip-data-transformed"
+  tags = {
+    Name = "${var.project_tag}"
+  }
 }
