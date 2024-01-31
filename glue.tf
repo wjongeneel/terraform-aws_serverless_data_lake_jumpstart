194aebdb34a3234b6cd25c1a0754365a107feb6c
willemjongeneel <willemjongeneel91@gmail.com>
2024-01-31 22:10:38 +0100

    update

---
+++ glue.tf
@@ -0,0 +1,16 @@
+resource "aws_glue_catalog_database" "nyctaxi-db" {
+  name = "nyctaxi-db"
+}
+
+resource "aws_glue_crawler" "nyc-taxi-yellow-trips-csv-crawler" {
+    name = "nyc-taxi-yellow-trips-csv-crawler"
+    database_name = aws_glue_catalog_database.nyctaxi-db.name
+    role = aws_iam_role.AWSGlueServiceRole-SDL-Jumpstart.arn
+    table_prefix = "raw_"
+    recrawl_policy {
+        recrawl_behavior = "CRAWL_EVERYTHING"
+    }
+   s3_target {
+    path = "s3://${aws_s3_bucket.taxi-trip-data-raw.bucket}/nyc-taxi/yellow-tripdata"
+  }   
+}
