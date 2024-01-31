a6fb4f5ecd739711962ea5a9c9c19a4af970cc43
willemjongeneel <willemjongeneel91@gmail.com>
2024-01-31 16:50:37 +0100

    first commit

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "tf-state-0001"
+    key            = "state/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+}
+}
